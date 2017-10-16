e07b7b5f01c5ae1a25dadb3a011f0f511dc9dc8c
Maratell <32012017+Maratell@users.noreply.github.com>
2017-10-16 13:25:43 +0400

    set webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './source/main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/
+            },
+            { test: /\.less$/, loader: "style-loader!css-loader!less-loader" }
+        ]
+    }
+
+};
+
